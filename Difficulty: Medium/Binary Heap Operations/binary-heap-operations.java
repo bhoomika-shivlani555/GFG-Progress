dd92786c233c5344ef6ed6ce0a3518c358637cd6
bhoomika-shivlani555 <bhoomikashivlani@gmail.com>
2026-08-19 18:38:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Binary Heap Operations/binary-heap-operations.java
@@ -0,0 +1,86 @@
+class MinHeap {
+    int[] harr;
+    int capacity;
+    int heap_size;
+
+    MinHeap(int cap) {
+        heap_size = 0;
+        capacity = cap;
+        harr = new int[cap];
+    }
+
+    int parent(int i) { return (i - 1) / 2; }
+
+    int left(int i) { return (2 * i + 1); }
+
+    int right(int i) { return (2 * i + 2); }
+
+    void insertKey(int k) {
+        if(heap_size>=capacity)
+            return;
+        decreaseKey(heap_size,k);
+        heap_size++;
+    }
+
+    int extractMin() {
+        if(heap_size<=0)
+            return -1;
+
+        if(heap_size==1)
+            return harr[--heap_size];
+
+        int ans=harr[0];
+        harr[0]=harr[heap_size-1];
+        heap_size--;
+        MinHeapify(0);
+
+        return ans;
+    }
+        
+    void deleteKey(int i) {
+        if(i>=heap_size || i<0)
+            return;
+        decreaseKey(i,Integer.MIN_VALUE);
+        extractMin();
+        
+    }
+
+    // Function to change value at ith index and store that value at first index.
+    void decreaseKey(int i, int new_val) {
+
+        // storing new value at ith index.
+        harr[i] = new_val;
+
+        while (i != 0 && harr[parent(i)] > harr[i]) {
+
+            // swapping values of ith index with its parent node
+            // if value at parent node is greater.
+            int temp = harr[i];
+            harr[i] = harr[parent(i)];
+            harr[parent(i)] = temp;
+            i = parent(i);
+        }
+    }
+
+    void MinHeapify(int i) {
+        int l = left(i);
+        int r = right(i);
+        int smallest = i;
+
+        // if left or right child is smaller than current element,
+        // we store its position.
+        if (l < heap_size && harr[l] < harr[smallest]) smallest = l;
+
+        if (r < heap_size && harr[r] < harr[smallest]) smallest = r;
+
+        // if smallest is not equal to i, we swap the values at their position.
+        if (smallest != i) {
+            int temp = harr[i];
+            harr[i] = harr[smallest];
+            harr[smallest] = temp;
+
+            // calling function recursively for the smallest index.
+            MinHeapify(smallest);
+        }
+    }
+}
